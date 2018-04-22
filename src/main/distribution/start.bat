54f79b27cce49dafd3d2646b2b3a49cd19fd5dda
Ayrat(bingo) <gpsdom@gmail.com>
2018-04-23 02:41:42 +0300

    wc

--- src/main/distribution/start.bat
+++ src/main/distribution/start.bat
@@ -2,10 +2,10 @@
 -server ^
 -XX:-UseParallelGC ^
 -Xms512M -Xmx2G ^
--Dcom.sun.management.jmxremote.port=1103 ^
+-Dcom.sun.management.jmxremote.port=${jmx-port} ^
 -Dcom.sun.management.jmxremote.authenticate=false ^
 -Dcom.sun.management.jmxremote.ssl=false ^
--agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=9093 ^
+-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${remote-debugger-port} ^
 -Dgosh.args=--noi ^
 -Duser.timezone=GMT+3 ^
 -cp "bin/felix.jar;lib/*" ^
