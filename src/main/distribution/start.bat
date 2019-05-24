cf341e878ae49a9e52aae474b6cf6018234e63ae
bio <gpsdom@gmail.com>
2019-05-24 19:50:19 +0300

    wc

--- src/main/distribution/start.bat
+++ src/main/distribution/start.bat
@@ -2,15 +2,16 @@
 -server ^
 -XX:-UseParallelGC ^
 -Xms512M -Xmx2G ^
--Dcom.sun.management.jmxremote.port=${jmx-port} ^
--Dcom.sun.management.jmxremote.authenticate=false ^
--Dcom.sun.management.jmxremote.ssl=false ^
 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${remote-debugger-port} ^
 -Dgosh.args=--noi ^
 -Duser.timezone=GMT+3 ^
 -cp "bin/felix.jar;lib/*" ^
 org.apache.felix.main.Main ^
 => start.log
+
+rem -Dcom.sun.management.jmxremote.port=${jmx-port} ^
+rem -Dcom.sun.management.jmxremote.authenticate=false ^
+rem -Dcom.sun.management.jmxremote.ssl=false ^
 
 rem -XX:MaxPermSize=128M ^
 rem -jar bin/felix.jar ^
